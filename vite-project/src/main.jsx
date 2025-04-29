2158f467359a2ae13a19910e0821d09e5dfc276a
alex <alex@quantifycrypto.net>
2025-04-29 02:32:59 -0400

    migrated vite

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,7 +1,7 @@
 // src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 
